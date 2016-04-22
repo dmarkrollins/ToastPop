c21ef0212d490517f83eac90aba6c61a1af6e226
David Rollins <drollins@sdcsol.com>
2016-04-21 22:36:22 -0400

    added rounded corners

--- Toaster/Form1.cs
+++ Toaster/Form1.cs
@@ -28,6 +28,18 @@ namespace Toaster
         private void cmdShowToast_Click(object sender, EventArgs e)
         {
             t = new Toast();
+
+            Rectangle Bounds = new Rectangle(0, 0, t.Width, t.Height);
+            int CornerRadius = 20;
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddArc(Bounds.X, Bounds.Y, CornerRadius, CornerRadius, 180, 90);
+            path.AddArc(Bounds.X + Bounds.Width - CornerRadius, Bounds.Y, CornerRadius, CornerRadius, 270, 90);
+            path.AddArc(Bounds.X + Bounds.Width - CornerRadius, Bounds.Y + Bounds.Height - CornerRadius, CornerRadius, CornerRadius, 0, 90);
+            path.AddArc(Bounds.X, Bounds.Y + Bounds.Height - CornerRadius, CornerRadius, CornerRadius, 90, 90);
+            path.CloseAllFigures();
+
+            t.Region = new Region(path);
+
             t.Show();
             t.fadeIn();
             cmdHideToast.Enabled = true;
